adce642fa6e65ae3c5f494941f6e505c1fc19081
semonbenyamen <semobenyamen@gmail.com>
2026-03-02 21:27:47 +0200

    comment

--- E-commerce-Product-Catalog--debugging/routes/productRoutes.js
+++ E-commerce-Product-Catalog--debugging/routes/productRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {createProduct, getAllProducts} = require("../controllers/productController");
+const {createProduct, getAllProducts, deleteProduct} = require("../controllers/productController");
 
+// Bug: Previously wrong HTTP method for create
+// Fix: Using POST for create
 router.post("/products", createProduct);
-router.get("/products/all", getAllProducts);
 
+// Bug: Route duplication
+// Fix: Clean REST structure
+router.get("/", getProducts);
+router.delete("/:id", deleteProduct);
 module.exports = router;
